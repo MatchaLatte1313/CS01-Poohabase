51cb665c9b61f1ca641b59cf86013ab079d5bfc9
MatchaLatte1313 <87053858+MatchaLatte1313@users.noreply.github.com>
2021-08-04 11:41:59 +0700

    Add files via upload

---
+++ CS01-10.py
@@ -0,0 +1,5 @@
+animal = ["Cat" , "Dog" , "Bird" , "Cat" , "Dog" , "Bird"] 
+for s in animal:
+    print(s)
+    if s == "Cat":
+        break
---
+++ CS01-9.py
@@ -0,0 +1,5 @@
+Name = [ "Preen" , "POOHBASE" , "Pooh"]
+for x in Name:
+ print(x)
+ if x == "POOHBASE":
+  break 
